14dea4ac9ca0e4833bfcbef8f39420b14bde2dc8
sirmcclay <mcclay.design@gmail.com>
2021-01-28 12:26:03 -0300

    list situations about run our apps in isolation and production

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,3 +3,15 @@ import faker from 'faker';
 const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
 document.querySelector('#cart-dev').innerHTML = cartText;
+
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of 'dev-cart'
+// We want to immediately render our app into that element
+
+// Context/Situation #2
+// We are running this file in development or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-cart' exists
+// WE DO NOT WANT try to immediately render the app
