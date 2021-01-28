01a0be718b12d2d893f54a61e8523e13cc4cfb5e
sirmcclay <mcclay.design@gmail.com>
2021-01-28 20:13:29 -0300

    refactor bootstrap to render immediately in isolation or export mount to container

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,17 +1,27 @@
 import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+	const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector('#cart-dev').innerHTML = cartText;
+	el.innerHTML = cartText;
+};
 
 // Context/Situation #1
 // We are running this file in development in isolation
 // We are using our local index.html file
 // Which DEFINITELY has an element with an id of 'dev-cart'
 // We want to immediately render our app into that element
+if (process.env.NODE_ENV === 'development') {
+	const el = document.querySelector('#cart-dev');
+
+	if (el) {
+		mount(el);
+	}
+}
 
 // Context/Situation #2
 // We are running this file in development or production
 // through the CONTAINER app
 // NO GUARANTEE that an element with an id of 'dev-cart' exists
 // WE DO NOT WANT try to immediately render the app
+export { mount };
